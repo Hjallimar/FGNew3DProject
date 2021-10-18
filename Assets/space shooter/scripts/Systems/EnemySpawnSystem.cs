214db6a9afeb37dfdb51c77a436a7f5547286245
Fredrik Henriksen Lövlie <fredriklovlie@gmail.com>
2021-10-18 16:44:54 +0200

    asdf

---
+++ Assets/space shooter/scripts/Systems/EnemySpawnSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class EnemySpawnSystem : SystemBase
+{
+    
+    
+    protected override void OnCreate()
+    {
+        var EnemySettings  = GetSingleton<EnemySettings>();
+        var EnemyInstances = new NativeArray<Entity>(EnemySettings.Count, Allocator.Temp);
+        EntityManager.Instantiate(EnemySettings.Prefab, EnemyInstances);
+
+        foreach (var e in EnemyInstances)
+        {
+            // EntityManager.SetComponentData(e, new Disabled{});
+        }
+
+        EnemyInstances.Dispose();
+    }
+
+    protected override void OnUpdate()
+    {
+       
+        // Enabled = false;
+    }
+}
