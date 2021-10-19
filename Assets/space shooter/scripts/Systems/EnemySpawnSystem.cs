35dd56f100114f2be739ef2e39869d81e10a5de8
Fredrik Henriksen Lövlie <fredriklovlie@gmail.com>
2021-10-19 15:43:56 +0200

    Update EnemySpawnSystem.cs

--- Assets/space shooter/scripts/Systems/EnemySpawnSystem.cs
+++ Assets/space shooter/scripts/Systems/EnemySpawnSystem.cs
@@ -1,39 +1,30 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
-using UnityEngine;
 
 public class EnemySpawnSystem : SystemBase
 {
-    private int priviet = 0;
-    
     protected override void OnCreate()
     {
-        var enemySettings  = GetSingleton<EnemySettings>();
+        EnemySettings enemySettings;
+        if (!TryGetSingleton(out enemySettings))
+        {
+            Enabled = false;
+            return;
+        }
+
         var EnemyInstances = new NativeArray<Entity>(enemySettings.Count, Allocator.Temp);
         EntityManager.Instantiate(enemySettings.Prefab, EnemyInstances);
-        
-        Debug.Log("JAG KAN SKRIVA HÄR ;)");
 
-        priviet++;
         foreach (var e in EnemyInstances)
         {
-            // EntityManager.SetComponentData(e, new Disabled{});
+            EntityManager.SetComponentData(e, new Disabled { });
         }
-        
-        Debug.Log("JAG KAN SKRIVA HÄR OCKSÅ ;)");
 
-        priviet++;
-        
         EnemyInstances.Dispose();
-        Debug.Log("JAG KAN SKRIVA HÄR ASDFASDF ;)");
-        
-        priviet++;
-        int i = 0;
     }
 
     protected override void OnUpdate()
     {
-       
         // Enabled = false;
     }
 }
